872083e8588f9cbcf2a49cca3b35046960aef0fb
Marius Cornescu <marius_cornescu@yahoo.com>
2020-02-03 17:55:25 +0100

    up

--- src/test/java/com/rtzan/camel/springboot/oc/KubernetesIntegrationKT.java
+++ src/test/java/com/rtzan/camel/springboot/oc/KubernetesIntegrationKT.java
@@ -18,9 +18,13 @@ package com.rtzan.camel.springboot.oc;
 
 import io.fabric8.kubernetes.client.KubernetesClient;
 
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -33,8 +37,14 @@ public class KubernetesIntegrationKT {
     @ArquillianResource
     KubernetesClient client;
 
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
     @Test
-    public void testAppProvisionsRunningPods() throws Exception {
+    public void testAppProvisionsRunningPods() {
         assertThat(client).deployments().pods().isPodReadyForPeriod();
     }
 }
